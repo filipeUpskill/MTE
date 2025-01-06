768ed7763957370c313b29834cf828f7848402d4
José Pinto <jjmppinto@gmail.com>
2025-01-06 15:50:41 +0000

    End Class Circle

--- src/main/java/pt/upskill/Circle.java
+++ src/main/java/pt/upskill/Circle.java
@@ -4,6 +4,28 @@ import java.awt.*;
 
 public class Circle {
 
+    private Point CenterPoint;
+    private double Radius;
 
 
+public Circle(Point CenterPoint, double Radius) {
+    this.CenterPoint = CenterPoint;
+    this.Radius = Radius;
+}
+public Point getCenterPoint() {
+    return CenterPoint;
+}
+public void setCenterPoint(Point CenterPoint) {
+    this.CenterPoint = CenterPoint;
+}
+public double getRadius() {
+    return Radius;
+}
+public void setRadius(double Radius) {
+    this.Radius = Radius;
+}
+
+    public double perimetro() {
+    return 6.28 * this.Radius;
+}
 }
