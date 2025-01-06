0c52a523c62b4bb05ad6fc35c48701149223183c
José Pinto <jjmppinto@gmail.com>
2025-01-06 16:29:54 +0000

    add c3 circle obj to main class.

--- src/main/java/pt/upskill/Main.java
+++ src/main/java/pt/upskill/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(2, 3);
+        Point p3 = new Point(4, 5);
         Circle c1 = new Circle(p1,12);
         Circle c2 = new Circle(p2,5);
+        Circle c3 = new Circle(p3,7);
 
 
     }
