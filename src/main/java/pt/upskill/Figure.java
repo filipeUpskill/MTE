3cd0d0c869467022608bec22cdbc26a200a97a53
José Pinto <jjmppinto@gmail.com>
2025-01-06 15:01:18 +0000

    Add atributes to Class Figure.

--- src/main/java/pt/upskill/Figure.java
+++ src/main/java/pt/upskill/Figure.java
@@ -1,6 +1,24 @@
 package pt.upskill;
 
 public class Figure {
-public String colour;
+
+private String color;
+
+public Figure(String color) {
+    this.color = color;
+}
+
+public String getColor() {
+    return color;
+}
+public void setColor(String color) {
+    this.color = color;
+}
+
+
+public abstract area();
+
+    public abstract perimetro() {
+}
 
 }
