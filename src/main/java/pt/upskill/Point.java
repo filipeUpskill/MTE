636690b92641a9b3ae5c64873f847ec31f324a0c
Vasco Pereira da Silva <902401149@portic.ipp.pt>
2025-01-06 15:07:06 +0000

    point update

--- src/main/java/pt/upskill/Point.java
+++ src/main/java/pt/upskill/Point.java
@@ -1,4 +1,24 @@
 package pt.upskill;
 
 public class Point {
+    public int xAxis;
+    public int yAxis;
+    public Point(int xAxis, int yAxis) {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+    }
+
+    public int getxAxis() {
+        return xAxis;
+    }
+    public int getyAxis() {
+        return yAxis;
+    }
+    public void setxAxis(int xAxis) {
+        this.xAxis = xAxis;
+    }
+    public void setyAxis(int yAxis) {
+        this.yAxis = yAxis;
+    }
+    
 }
