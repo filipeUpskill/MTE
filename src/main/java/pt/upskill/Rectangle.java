800ca7ae2307bc8b4ead1c2bc36138f1c236d810
Filipe Costa <902401076@portic.ipp.pt>
2025-01-06 15:10:16 +0000

    Added variable to Rectangle

--- src/main/java/pt/upskill/Rectangle.java
+++ src/main/java/pt/upskill/Rectangle.java
@@ -5,4 +5,26 @@ import java.awt.*;
 public class Rectangle {
 public Point UpperLeft;
 public Point BottomRight;
-}
+
+    public Rectangle(Point upperLeft, Point bottomRight) {
+        this.UpperLeft = upperLeft;
+        this.BottomRight = bottomRight;
+    }
+
+        public Point getUpperLeft () {
+            return this.UpperLeft;
+        }
+
+        public void setUpperLeft (Point upperLeft){
+            this.UpperLeft = upperLeft;
+        }
+
+        public Point getBottomRight () {
+            return this.BottomRight;
+        }
+
+        public void setBottomRight (Point bottomRight){
+            this.BottomRight = bottomRight;
+        }
+    }
+
