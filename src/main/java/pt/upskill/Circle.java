dbf82319363a8fb6efed81a6323c218877672261
Vasco Pereira da Silva <902401149@portic.ipp.pt>
2025-01-06 16:02:16 +0000

    Adicionada a Area ao Circulo

--- src/main/java/pt/upskill/Circle.java
+++ src/main/java/pt/upskill/Circle.java
@@ -8,24 +8,32 @@ public class Circle {
     private double Radius;
 
 
-public Circle(Point CenterPoint, double Radius) {
-    this.CenterPoint = CenterPoint;
-    this.Radius = Radius;
-}
-public Point getCenterPoint() {
-    return CenterPoint;
-}
-public void setCenterPoint(Point CenterPoint) {
-    this.CenterPoint = CenterPoint;
-}
-public double getRadius() {
-    return Radius;
-}
-public void setRadius(double Radius) {
-    this.Radius = Radius;
-}
+    public Circle(Point CenterPoint, double Radius) {
+        this.CenterPoint = CenterPoint;
+        this.Radius = Radius;
+    }
+
+    public Point getCenterPoint() {
+        return CenterPoint;
+    }
+
+    public void setCenterPoint(Point CenterPoint) {
+        this.CenterPoint = CenterPoint;
+    }
+
+    public double getRadius() {
+        return Radius;
+    }
+
+    public void setRadius(double Radius) {
+        this.Radius = Radius;
+    }
 
     public double perimetro() {
-    return 6.28 * this.Radius;
-}
+        return 6.28 * this.Radius;
+    }
+
+    public double area() {
+        return Math.PI * this.Radius * this.Radius;
+    }
 }
